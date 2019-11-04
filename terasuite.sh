4db38715e001dc12e2784b87f30064416878b3e2
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-11-04 14:15:53 -0800

    allow multiple size parameters

--- terasuite.sh
+++ terasuite.sh
@@ -11,7 +11,7 @@ trap "" HUP
 function usage () {
   cat << EOF
 
-  Usage: $0 <1G|10G|50G|500G|1T>
+  Usage: $0 <1G|10G|50G|500G|1T|10T|100T>
 
     --help    shows this help menu
 
@@ -64,15 +64,20 @@ shift $((OPTIND-1))
 # VALIDATION
 #------------------------------------------------------------------------------
 
-# validate sample size
+# validate parameters
+declare -A SIZES
 if [[ -z ${1+x} ]]; then
-  SIZE="1T"
-elif [[ $1 =~ ^(1G|10G|100G|500G|1T)$ ]]; then
-  SIZE=$1
+  SIZES+=("1T")
 else
-  echo "==> ERROR: An unsupported sample size was requested"
-  usage;
-  exit 1;
+  for i in $@; do
+    if [[ $i =~ ^(1G|10G|100G|500G|1T|10T|100T)$ ]]; then
+      SIZES+=($i)
+    else
+      echo "==> ERROR: Unsupported sample size '${i}' was requested"
+      usage;
+      exit 1;
+    fi
+  done
 fi
 
 #------------------------------------------------------------------------------
@@ -96,9 +101,7 @@ DATE=`date +%Y%m%d`
 TIME=`date +%H%M%S`
 LOGDIR="./logs"
 RESULTDIR="${LOGDIR}/TeraSort/${DATE}"
-SAMPLE_DATASET="/benchmarks/TeraSort/${DATE}${TIME}/teragen_sample_${SIZE}"
-SORTED_DATASET="/benchmarks/TeraSort/${DATE}${TIME}/terasort_sorted_${SIZE}"
-VALIDATED_DATASET="/benchmarks/TeraSort/${DATE}${TIME}/teravalidate_report_${SIZE}"
+TERASORT_PREFIX="/benchmarks/TeraSort/${DATE}${TIME}"
 
 #------------------------------------------------------------------------------
 # MAIN
@@ -109,88 +112,95 @@ if [ ! -d "${RESULTDIR}" ]; then
   mkdir -p ${RESULTDIR}
 fi
 
-# kill running mapreduce jobs
-mrkill
+for size in ${SIZES}; do
+  # define datasets
+  SAMPLE_DATASET="${TERASORT_PREFIX}/${size}_teragen_sample"
+  SORTED_DATASET="${TERASORT_PREFIX}/${size}_terasort_sorted"
+  VALIDATED_DATASET="${TERASORT_PREFIX}/${size}_teravalidate_report"
 
-# run teragen
-echo "==> TeraGen Phase"
-time hadoop jar ${MR_EXAMPLES_JAR} teragen \
-  -Dmapreduce.map.log.level=INFO \
-  -Dmapreduce.reduce.log.level=INFO \
-  -Dyarn.app.mapreduce.am.log.level=INFO \
-  -Dio.file.buffer.size=131072 \
-  -Dmapreduce.map.cpu.vcores=1 \
-  -Dmapreduce.map.java.opts=-Xmx1536m \
-  -Dmapreduce.map.maxattempts=1 \
-  -Dmapreduce.map.memory.mb=2048 \
-  -Dmapreduce.map.output.compress=true \
-  -Dmapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.Lz4Codec \
-  -Dmapreduce.reduce.cpu.vcores=1 \
-  -Dmapreduce.reduce.java.opts=-Xmx1536m \
-  -Dmapreduce.reduce.maxattempts=1 \
-  -Dmapreduce.reduce.memory.mb=2048 \
-  -Dmapreduce.task.io.sort.factor=100 \
-  -Dmapreduce.task.io.sort.mb=384 \
-  -Dyarn.app.mapreduce.am.command.opts=-Xmx768m \
-  -Dyarn.app.mapreduce.am.resource.mb=1024 \
-  -Dmapred.map.tasks=92 \
-  ${ROWS[${SIZE}]} ${SAMPLE_DATASET} >> "${RESULTDIR}/teragen_results_${DATE}${TIME}.txt" 2>&1
+  # kill running mapreduce jobs
+  mrkill
 
-# verify teragen dataset exists
-if ! hdfs dfs -ls ${SAMPLE_DATASET} > /dev/null 2>&1; then
-  echo "==> ERROR: TeraGen dataset directory '${SAMPLE_DATASET}' not found"
-  exit 1;
-fi
+  # run teragen
+  echo "==> TeraGen Phase"
+  time hadoop jar ${MR_EXAMPLES_JAR} teragen \
+    -Dmapreduce.map.log.level=INFO \
+    -Dmapreduce.reduce.log.level=INFO \
+    -Dyarn.app.mapreduce.am.log.level=INFO \
+    -Dio.file.buffer.size=131072 \
+    -Dmapreduce.map.cpu.vcores=1 \
+    -Dmapreduce.map.java.opts=-Xmx1536m \
+    -Dmapreduce.map.maxattempts=1 \
+    -Dmapreduce.map.memory.mb=2048 \
+    -Dmapreduce.map.output.compress=true \
+    -Dmapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.Lz4Codec \
+    -Dmapreduce.reduce.cpu.vcores=1 \
+    -Dmapreduce.reduce.java.opts=-Xmx1536m \
+    -Dmapreduce.reduce.maxattempts=1 \
+    -Dmapreduce.reduce.memory.mb=2048 \
+    -Dmapreduce.task.io.sort.factor=100 \
+    -Dmapreduce.task.io.sort.mb=384 \
+    -Dyarn.app.mapreduce.am.command.opts=-Xmx768m \
+    -Dyarn.app.mapreduce.am.resource.mb=1024 \
+    -Dmapred.map.tasks=92 \
+    ${ROWS[${size}]} ${SAMPLE_DATASET} >> "${RESULTDIR}/teragen_results_${DATE}${TIME}_${size}.txt" 2>&1
 
-# kill running mapreduce jobs
-mrkill
+  # verify teragen dataset exists
+  if ! hdfs dfs -ls ${SAMPLE_DATASET} > /dev/null 2>&1; then
+    echo "==> ERROR: TeraGen dataset directory '${SAMPLE_DATASET}' not found"
+    exit 1;
+  fi
 
-# run terasort
-echo "==> TeraSort Phase"
-time hadoop jar ${MR_EXAMPLES_JAR} terasort \
-  -Dmapreduce.map.log.level=INFO \
-  -Dmapreduce.reduce.log.level=INFO \
-  -Dyarn.app.mapreduce.am.log.level=INFO \
-  -Dio.file.buffer.size=131072 \
-  -Dmapreduce.map.cpu.vcores=1 \
-  -Dmapreduce.map.java.opts=-Xmx1536m \
-  -Dmapreduce.map.maxattempts=1 \
-  -Dmapreduce.map.memory.mb=2048 \
-  -Dmapreduce.map.output.compress=true \
-  -Dmapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.Lz4Codec \
-  -Dmapreduce.reduce.cpu.vcores=1 \
-  -Dmapreduce.reduce.java.opts=-Xmx1536m \
-  -Dmapreduce.reduce.maxattempts=1 \
-  -Dmapreduce.reduce.memory.mb=2048 \
-  -Dmapreduce.task.io.sort.factor=300 \
-  -Dmapreduce.task.io.sort.mb=384 \
-  -Dyarn.app.mapreduce.am.command.opts=-Xmx768m \
-  -Dyarn.app.mapreduce.am.resource.mb=1024 \
-  -Dmapred.reduce.tasks=92 \
-  -Dmapreduce.terasort.output.replication=1 \
-  ${SAMPLE_DATASET} ${SORTED_DATASET} >> "${RESULTDIR}/terasort_results_${DATE}${TIME}.txt" 2>&1
+  # kill running mapreduce jobs
+  mrkill
 
-# verify terasort output exists
-if ! hdfs dfs -ls ${SORTED_DATASET} > /dev/null 2>&1; then
-  echo "==> ERROR: TeraSort output directory '${SORTED_DATASET}' not found"
-  exit 1;
-fi
+  # run terasort
+  echo "==> TeraSort Phase"
+  time hadoop jar ${MR_EXAMPLES_JAR} terasort \
+    -Dmapreduce.map.log.level=INFO \
+    -Dmapreduce.reduce.log.level=INFO \
+    -Dyarn.app.mapreduce.am.log.level=INFO \
+    -Dio.file.buffer.size=131072 \
+    -Dmapreduce.map.cpu.vcores=1 \
+    -Dmapreduce.map.java.opts=-Xmx1536m \
+    -Dmapreduce.map.maxattempts=1 \
+    -Dmapreduce.map.memory.mb=2048 \
+    -Dmapreduce.map.output.compress=true \
+    -Dmapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.Lz4Codec \
+    -Dmapreduce.reduce.cpu.vcores=1 \
+    -Dmapreduce.reduce.java.opts=-Xmx1536m \
+    -Dmapreduce.reduce.maxattempts=1 \
+    -Dmapreduce.reduce.memory.mb=2048 \
+    -Dmapreduce.task.io.sort.factor=300 \
+    -Dmapreduce.task.io.sort.mb=384 \
+    -Dyarn.app.mapreduce.am.command.opts=-Xmx768m \
+    -Dyarn.app.mapreduce.am.resource.mb=1024 \
+    -Dmapred.reduce.tasks=92 \
+    -Dmapreduce.terasort.output.replication=1 \
+    ${SAMPLE_DATASET} ${SORTED_DATASET} >> "${RESULTDIR}/terasort_results_${DATE}${TIME}_${size}.txt" 2>&1
 
-# kill running mapreduce jobs
-mrkill
+  # verify terasort output exists
+  if ! hdfs dfs -ls ${SORTED_DATASET} > /dev/null 2>&1; then
+    echo "==> ERROR: TeraSort output directory '${SORTED_DATASET}' not found"
+    exit 1;
+  fi
 
-# run teravalidate
-echo "==> TeraValidate Phase"
-time hadoop jar ${MR_EXAMPLES_JAR} teravalidate \
-  -Ddfs.blocksize=256M \
-  -Dio.file.buffer.size=131072 \
-  -Dmapreduce.map.memory.mb=2048 \
-  -Dmapreduce.map.java.opts=-Xmx1536m \
-  -Dmapreduce.reduce.memory.mb=2048 \
-  -Dmapreduce.reduce.java.opts=-Xmx1536m \
-  -Dyarn.app.mapreduce.am.resource.mb=1024 \
-  -Dyarn.app.mapreduce.am.command-opts=-Xmx768m \
-  -Dmapreduce.task.io.sort.mb=1 \
-  -Dmapred.map.tasks=185 \
-  -Dmapred.reduce.tasks=185 \
-  ${SORTED_DATASET} ${VALIDATED_DATASET} >> "${RESULTDIR}/teravalidate_results_${DATE}${TIME}.txt" 2>&1
+  # kill running mapreduce jobs
+  mrkill
+
+  # run teravalidate
+  echo "==> TeraValidate Phase"
+  time hadoop jar ${MR_EXAMPLES_JAR} teravalidate \
+    -Ddfs.blocksize=256M \
+    -Dio.file.buffer.size=131072 \
+    -Dmapreduce.map.memory.mb=2048 \
+    -Dmapreduce.map.java.opts=-Xmx1536m \
+    -Dmapreduce.reduce.memory.mb=2048 \
+    -Dmapreduce.reduce.java.opts=-Xmx1536m \
+    -Dyarn.app.mapreduce.am.resource.mb=1024 \
+    -Dyarn.app.mapreduce.am.command-opts=-Xmx768m \
+    -Dmapreduce.task.io.sort.mb=1 \
+    -Dmapred.map.tasks=185 \
+    -Dmapred.reduce.tasks=185 \
+    ${SORTED_DATASET} ${VALIDATED_DATASET} >> "${RESULTDIR}/teravalidate_results_${DATE}${TIME}_${size}.txt" 2>&1
+done
