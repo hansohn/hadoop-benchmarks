b6518c6d92733534397d7c34a887f972e592a107
Ryan Hansohn <hansohn@users.noreply.github.com>
2019-11-04 16:40:42 -0800

    update output path

--- testdfsio.sh
+++ testdfsio.sh
@@ -14,9 +14,9 @@ FILESIZE=100000
 DATE=`date +%Y%m%d`
 TIME=`date +%H%M%S`
 LOGDIR="./logs"
-RESULTDIR="${LOGDIR}/TestDFSIO/${DATE}"
-DFSIO_WRITE_OUTPUT_FILE="${RESULTDIR}/dfsio_write_results_${DATE}${TIME}.txt"
-DFSIO_READ_OUTPUT_FILE="${RESULTDIR}/dfsio_read_results_${DATE}${TIME}.txt"
+RESULTDIR="${LOGDIR}/TestDFSIO/${DATE}/${TIME}"
+DFSIO_WRITE="${RESULTDIR}/dfsio_write.txt"
+DFSIO_READ="${RESULTDIR}/dfsio_read.txt"
 
 #------------------------------------------------------------------------------
 # MAIN
@@ -31,17 +31,16 @@ fi
 echo "==> TestDFSIO Clean Phase"
 yarn jar ${DFSIO_JAR} TestDFSIO -clean
 
-
 # testdfsio write
 echo "==> TestDFSIO Write Phase"
 yarn jar ${DFSIO_JAR} TestDFSIO \
   -write -nrFiles ${FILES} \
   -fileSize ${FILESIZE} \
-  -resFile ${DFSIO_WRITE_OUTPUT_FILE}
+  -resFile ${DFSIO_WRITE}
 
 # testdfsio read
 echo "==> TestDFSIO Read Phase"
 yarn jar ${DFSIO_JAR} TestDFSIO \
   -read -nrFiles ${FILES} \
   -fileSize ${FILESIZE} \
-  -resFile ${DFSIO_READ_OUTPUT_FILE}
+  -resFile ${DFSIO_READ}
